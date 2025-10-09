b528011610c0736cea47a515c703672e0f3105fa
Louis Remes <>
2025-10-09 14:58:31 +0200

    V0.2 file protection

--- setup_env.sh
+++ setup_env.sh
@@ -4,4 +4,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+chmod 555 provided_code code_to_be_implemented/__init__.py main.py
+rm requirements.txt setup_env.ps1 setup_env.sh
 echo "✅ Environment ready. Activate with: source .venv/bin/activate"
